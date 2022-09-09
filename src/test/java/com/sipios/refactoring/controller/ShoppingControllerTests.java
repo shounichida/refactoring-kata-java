d504159c56eb17ff31140d8fe573234d8737c30c
Lionel Tougne <lgt_23@msn.com>
2022-09-09 09:49:24 +0200

    Pour ce time boxe le jeux de test ne sera pas complet
    Mais l'idée est je teste differente valeur avec different type d'item et je vais cherche les cas limite (dans le code j'ai vu des exceptions)
    
    Là ici je n'ai le temps de que de teste des tshirts et le test du msg d'exception ne marche pas.
    
    Malgrè que ce soit pas assez ceinture et bretelle pour moi (normal c'est un exo limite en temps) j'attaque le refacto.

--- src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -3,9 +3,12 @@ package com.sipios.refactoring.controller;
 import com.sipios.refactoring.UnitTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ShoppingControllerTests extends UnitTest {
 
@@ -19,37 +22,68 @@ class ShoppingControllerTests extends UnitTest {
         );
     }
 
-    @Test
-    void whenStandardCustomerWithTShirtReturn30() {
-        final Item[] items = {new Item("TSHIRT",1)};
-        final String price = controller.getPrice(new Body(items, "STANDARD_CUSTOMER"));
-        System.out.println("--->>>"+price);
+    @ParameterizedTest
+    @CsvSource(value = {"1:30.0","2:60.0","3:90.0","4:120.0" ,"5:150.0"}, delimiter = ':')
+    void whenStandardCustomerWithTShirtReturnAValue(String quantity,String priceExpected) {
+        final Item[] items = {new Item("TSHIRT", Integer.parseInt(quantity))};
 
 
-      assertEquals("30.0",
+      assertEquals(priceExpected,
             controller.getPrice(new Body(items, "STANDARD_CUSTOMER"))
         );
     }
 
-    @Test
-    void whenPremiumCustomerReturn0() {
-        final String price = controller.getPrice(new Body(new Item[]{}, "PREMIUM_CUSTOMER"));
-        System.out.println("--->>>"+price);
+    @ParameterizedTest
+    @CsvSource(value = {"7:toto","7:toto"}, delimiter = ':')
+    void whenStandardCustomerWithNbTShirttohightReturnAnException(String quantity,String exceptionExpected) {
+        final Item[] items = {new Item("TSHIRT", Integer.parseInt(quantity))};
 
 
-        assertEquals("0.0",
-            controller.getPrice(new Body(new Item[] {}, "PREMIUM_CUSTOMER"))
+        assertThrows(Exception.class,
+            ()-> controller.getPrice(new Body(items, "STANDARD_CUSTOMER")),exceptionExpected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:27.0","2:54.0","3:81.0","4:108.0" ,"5:135.0"}, delimiter = ':')
+    void whenPremiumCustomerReturnValue(String quantity,String priceExpected) {
+        final Item[] items = {new Item("TSHIRT", Integer.parseInt(quantity))};
+
+
+        assertEquals(priceExpected,
+            controller.getPrice(new Body(items, "PREMIUM_CUSTOMER"))
         );
     }
 
-    @Test
-    void whenPlatiniumCustomerReturn0() {
-        final String price = controller.getPrice(new Body(new Item[]{}, "PLATINUM_CUSTOMER"));
-        System.out.println("--->>>"+price);
+    @ParameterizedTest
+    @CsvSource(value = {"120:toto","200:toto"}, delimiter = ':')
+    void whenPremiumCustomerWithNbTShirttohightReturnAnException(String quantity,String exceptionExpected) {
+        final Item[] items = {new Item("TSHIRT", Integer.parseInt(quantity))};
 
 
-        assertEquals("0.0",
-            controller.getPrice(new Body(new Item[] {}, "PLATINUM_CUSTOMER"))
+        assertThrows(Exception.class,
+            ()-> controller.getPrice(new Body(items, "PREMIUM_CUSTOMER")),exceptionExpected);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:15.0","2:30.0","3:45.0","4:60.0" ,"5:75.0"}, delimiter = ':')
+    void whenPlatiniumCustomerReturnValue(String quantity,String priceExpected) {
+        final Item[] items = {new Item("TSHIRT", Integer.parseInt(quantity))};
+
+
+        assertEquals(priceExpected,
+            controller.getPrice(new Body(items, "PLATINUM_CUSTOMER"))
         );
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"200:toto","300:toto"}, delimiter = ':')
+    void whenPlatiniumCustomerWithNbTShirttohightReturnAnException(String quantity,String exceptionExpected) {
+        final Item[] items = {new Item("TSHIRT", Integer.parseInt(quantity))};
+
+
+        assertThrows(Exception.class,
+            ()-> controller.getPrice(new Body(items, "PLATINUM_CUSTOMER")),exceptionExpected);
+    }
+
 }
